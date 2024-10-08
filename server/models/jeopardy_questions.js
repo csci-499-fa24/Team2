d857dd3a5458136cf7d3eccecbfa7387dba7d658
Michael Russo <michaelrusso242@aol.com>
2024-10-08 14:39:11 -0400

    Fix the sequelize model to work with the table we're using

--- server/models/jeopardy_questions.js
+++ server/models/jeopardy_questions.js
@@ -3,40 +3,42 @@ module.exports = (sequelize, DataTypes) => {
     ShowNumber: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true, // Set ShowNumber as the primary key
+      primaryKey: true,
+      field: 'ShowNumber' // Ensure that Sequelize uses the correct column name
     },
     AirDate: {
       type: DataTypes.DATE,
-      allowNull: true, // Assuming it can be null
+      allowNull: true,
+      field: 'AirDate' // Explicitly tell Sequelize the column name
     },
     Round: {
       type: DataTypes.STRING,
       allowNull: false,
+      field: 'Round' // Explicitly tell Sequelize the column name
     },
     Category: {
       type: DataTypes.STRING,
       allowNull: false,
+      field: 'Category' // Explicitly tell Sequelize the column name
     },
     Value: {
       type: DataTypes.STRING,
       allowNull: false,
+      field: 'Value' // Explicitly tell Sequelize the column name
     },
     Question: {
       type: DataTypes.TEXT,
       allowNull: false,
+      field: 'Question' // Explicitly tell Sequelize the column name
     },
     Answer: {
       type: DataTypes.TEXT,
       allowNull: false,
+      field: 'Answer' // Explicitly tell Sequelize the column name
     }
   }, {
     tableName: 'jeopardy_questions',
-    timestamps: false,
-    // Disable the default 'id' field
-    underscored: true,
-    freezeTableName: true,
-    createdAt: false,
-    updatedAt: false
+    timestamps: false // Disable automatic timestamps
   });
 
   return JeopardyQuestions;
