c430b9844e12c5bb06b31a7de96ab6292eded4c1
Michael Russo <michaelrusso242@aol.com>
2024-10-21 21:51:47 -0400

    Attempted fix for the game api

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const gameRoutes = require('./games');
 const JeopardyController = require('./jeopardy');
 const verifyToken = require('./verifyToken');
 const checkExistingUser = require('./checkExistingUser');
@@ -7,5 +8,6 @@ const checkExistingUser = require('./checkExistingUser');
 router.use('/jeopardy', JeopardyController);
 router.use('/verifyToken', verifyToken);
 router.use('/checkExistingUser', checkExistingUser);
+router.use('/games', gameRoutes);
 
 module.exports = router;
