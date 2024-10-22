91de99969b588ee38710411aa013c10a2bdd00dc
Michael Russo <michaelrusso242@aol.com>
2024-10-21 21:09:49 -0400

    Ensures that the socket displayName is updated as well

--- client/app/[userid]/profile/page.js
+++ client/app/[userid]/profile/page.js
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
-import { updateDisplayName, updateUserEmail} from "../../redux/authSlice";
+import { updateDisplayName, updateUserEmail } from "../../redux/authSlice";
 import styles from './profile.module.css';
 import Navbar from '@/app/components/navbar';
 import ProtectedRoute from '@/app/components/protectedRoute';
@@ -19,30 +19,31 @@ const ProfilePage = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if(!user && !loading) {
+        if (!user && !loading) {
             router.push("/");
-        }else if (user) {
+        } else if (user) {
             setUserEmail(user.email);
             setDisplayName(user.displayName);
         }
     }, [userid, user]);
 
-    const handleForm = async(e) => {
+    const handleForm = async (e) => {
         e.preventDefault();
-        const emailToUpdate = newEmail ===  "" ? userEmail : newEmail;
+        const emailToUpdate = newEmail === "" ? userEmail : newEmail;
         const displayNameToUpdate = newDisplayName === "" ? displayName : newDisplayName;
         let updatedName = false;
         let updatedEmail = false;
 
-        if(displayNameToUpdate !== displayName || emailToUpdate !== userEmail) {
-            if(displayNameToUpdate !== displayName) {
+        if (displayNameToUpdate !== displayName || emailToUpdate !== userEmail) {
+            if (displayNameToUpdate !== displayName) {
                 try {
                     console.log("update in progress");
                     const isDisplayNameUpdated = await dispatch(updateDisplayName(userid, displayNameToUpdate));
                     console.log("updatedDisplayName:", isDisplayNameUpdated);
                     if (isDisplayNameUpdated) {
                         updatedName = true;
-                    }else {
+                        setDisplayName(user.newDisplayName);
+                    } else {
                         updatedName = false;
                     }
                 } catch (error) {
@@ -50,12 +51,12 @@ const ProfilePage = () => {
                 }
             }
 
-            if(emailToUpdate !== userEmail) {
+            if (emailToUpdate !== userEmail) {
                 try {
                     const isEmailUpdated = await dispatch(updateUserEmail(userid, emailToUpdate));
                     if (isEmailUpdated) {
                         updatedEmail = true
-                    }else {
+                    } else {
                         updatedEmail = false
                     }
                 } catch (error) {
@@ -63,40 +64,40 @@ const ProfilePage = () => {
                 }
             }
 
-            if(updatedName || updatedEmail) {
+            if (updatedName || updatedEmail) {
                 alert("Profile updated successfully.");
-            } else if (!updatedName){
+            } else if (!updatedName) {
                 alert("Display name already exists. Please choose another one.");
-            } else if (!updatedEmail){
+            } else if (!updatedEmail) {
                 alert("Email already exists. Please choose another one.");
             }
 
-        }else {
+        } else {
             alert("No changes made.");
         }
     }
-    
+
     return (
         // <ProtectedRoute>
-            <div>
-                <Navbar />
-                <div className={styles.profileContainer}>
-                    <h2 className={styles.profileGreeting}>Hello, {displayName}!</h2>
-                    <form className={styles.formContainer}>
-                        <label htmlFor="email" className={styles.inputLabel}>Email:</label>
-                        <input type="email" name="email" id="email" disabled className={styles.inputField + " " + styles.disabledInput}
-                        placeholder={userEmail} onChange={(e) => setNewEmail(e.target.value)}/>
+        <div>
+            <Navbar />
+            <div className={styles.profileContainer}>
+                <h2 className={styles.profileGreeting}>Hello, {displayName}!</h2>
+                <form className={styles.formContainer}>
+                    <label htmlFor="email" className={styles.inputLabel}>Email:</label>
+                    <input type="email" name="email" id="email" disabled className={styles.inputField + " " + styles.disabledInput}
+                        placeholder={userEmail} onChange={(e) => setNewEmail(e.target.value)} />
 
-                        <label htmlFor="displayName" className={styles.inputLabel}>Display Name:</label>
-                        <input type="text" name="displayName" id="displayName" className={styles.inputField}
-                        placeholder={displayName} onChange={(e) => setNewDisplayName(e.target.value)}/>
-                    </form>
-                    <div className={styles.buttonsContainer}>
+                    <label htmlFor="displayName" className={styles.inputLabel}>Display Name:</label>
+                    <input type="text" name="displayName" id="displayName" className={styles.inputField}
+                        placeholder={displayName} onChange={(e) => setNewDisplayName(e.target.value)} />
+                </form>
+                <div className={styles.buttonsContainer}>
                     <button className={styles.buttons} onClick={handleForm} >Update Profile</button>
                     <button className={styles.buttons} onClick={() => router.push(`/${userid}`)}>Go back</button>
-                    </div>
                 </div>
             </div>
+        </div>
         // </ProtectedRoute> 
     );
 };
