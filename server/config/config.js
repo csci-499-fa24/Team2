32da4035a1168d0953613c1a66876014352490e4
Shelly Masih <shelly_masih22@yahoo.com>
2024-10-21 23:59:21 -0400

    rerouted ready button

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "development": {
       "username": "root",
-      "password": "root",
+      "password": ";{:\@nmv}NFslH-E",
       "database": "jeopardy",
       "host": "127.0.0.1",
       "port": 3306,
