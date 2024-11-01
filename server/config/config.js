cd723b1825f739d9a1351d2ea9b1f9167256a1d1
Tasmina Hassan <tasmina.hassan10@myhunter.cuny.edu>
2024-11-01 18:05:07 -0400

    improved answer validation

--- server/config/config.js
+++ server/config/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-    "development": {
-      "username": "root",
-      "password": "",
-      "database": "jeopardy",
-      "host": "127.0.0.1",
-      "port": 3306,
-      "dialect": "mysql"
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
   },
-  "test": {
-    "username": "root",
-      "password": "",
-      "database": "jeopardy",
-      "host": "127.0.0.1",
-      "port": 3306,
-      "dialect": "mysql"
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
   },
-  "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  },
+};
