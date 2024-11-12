95185a38fc357d31fb9f056a085f8dbbaad42ca9
alejobotero <102490384+alejobotero@users.noreply.github.com>
2024-11-12 01:31:56 -0500

    s

--- server/controllers/history.js
+++ server/controllers/history.js
@@ -16,9 +16,8 @@ router.get('/player_history/:playerId', async (req, res) => {
     }
 });
 
-// New route to end a game
 router.post('/record_game', async (req, res) => {
-    console.log("Request received to record game:", req.body);
+    console.log("Request received to record game:", req.body); // Log received data
 
     const {
         gameId,
@@ -31,6 +30,7 @@ router.post('/record_game', async (req, res) => {
 
     // Validate input
     if (!gameId || !showNumber || !owner || !winner || points === undefined || !Array.isArray(players)) {
+        console.log("Invalid request data:", req.body); // Log invalid data for debugging
         return res.status(400).json({ error: 'Missing required fields or invalid data' });
     }
 
@@ -39,8 +39,9 @@ router.post('/record_game', async (req, res) => {
         res.status(200).json({ message: 'Game recorded successfully' });
     } catch (error) {
         console.error('Error ending game:', error);
-        res.status(500).json({ error: 'An error occurred while record the game' });
+        res.status(500).json({ error: 'An error occurred while recording the game' });
     }
 });
+
 
 module.exports = router;
