b47cce32ce452cea47dde42adde4d158d030bf96
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2024-11-18 12:51:16 -0500

    removed deleted file import

--- client/app/layout.js
+++ client/app/layout.js
@@ -2,7 +2,6 @@
 import React, {useEffect} from "react";
 import localFont from "next/font/local";
 import "./globals.css";
-import ReduxProvider from "./ReduxProvider";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { store } from "./redux/store";
 import { monitorAuthState, updateUserStatus } from "./redux/authSlice";
